69a46745e61ccefb0870d847c1a48c60ea577187
davidenglishmusic <david@operaonfilm.com>
2017-11-19 15:31:01 -0400

    adds a row composer class

---
+++ lib/row_composer.rb
@@ -0,0 +1,18 @@
+require_relative 'row'
+
+class RowComposer
+  TONES = [
+    [0, 'c'],
+    [1, 'c#'],
+    [2, 'd'],
+    [3, 'eb'],
+    [4, 'e'],
+    [5, 'f'],
+    [6, 'f#'],
+    [7, 'g'],
+    [8, 'ab'],
+    [9, 'a'],
+    [10, 'bb'],
+    [11, 'b']
+  ].freeze
+end
