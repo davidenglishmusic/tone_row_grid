cc6df33c8c90d1d8e12e9c7bc6e96591855bd650
davidenglishmusic <david@operaonfilm.com>
2017-11-19 15:28:41 -0400

    implements row interval sequence

--- lib/row.rb
+++ lib/row.rb
@@ -4,4 +4,12 @@ class Row
   def initialize(row)
     @row = row
   end
+
+  def interval_sequence
+    intervals = []
+    @row.each_cons(2) do |a, b|
+      intervals.push(b - a)
+    end
+    intervals
+  end
 end
