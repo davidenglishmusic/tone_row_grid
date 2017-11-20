c5ec1a3dee06cf6a691286da9daefbf65b0bd273
davidenglishmusic <david@operaonfilm.com>
2017-11-20 19:49:39 -0400

    uses row composer in app

--- app.rb
+++ app.rb
@@ -1,37 +1,16 @@
-tones = [
-  [0, 'c'],
-  [1, 'c#'],
-  [2, 'd'],
-  [3, 'eb'],
-  [4, 'e'],
-  [5, 'f'],
-  [6, 'f#'],
-  [7, 'g'],
-  [8, 'ab'],
-  [9, 'a'],
-  [10, 'bb'],
-  [11, 'b']
-]
+require_relative 'lib/row_composer'
 
-primary_row = tones.shuffle.map { |tone| tone[0] }
+seed = RowComposer.generate_random_row
 
 intervals = []
-primary_row.each_cons(2) do |a, b|
+seed.row.each_cons(2) do |a, b|
   intervals.push(b - a)
 end
 
-direct_inversion = [primary_row.first]
-
-next_note = primary_row.first
-intervals.each do |interval|
-  next_note -= interval
-  next_note += 12 if next_note.negative?
-  next_note -= 12 if next_note >= 12
-  direct_inversion.push(next_note)
-end
+direct_inversion = RowComposer.inverse(seed)
 
 rows = []
-direct_inversion.each do |pitch|
+direct_inversion.row.each do |pitch|
   next_note = pitch
   row = [pitch]
   intervals.each do |interval|
