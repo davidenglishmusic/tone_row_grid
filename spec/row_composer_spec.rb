7c47b1662337563765e2329ac7f036b2ddbfa71b
davidenglishmusic <david@operaonfilm.com>
2017-11-26 20:18:57 -0400

    specs out row transformation labels

--- spec/row_composer_spec.rb
+++ spec/row_composer_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe RowComposer do
       row = Row.new([7, 11, 3, 2, 6, 9, 8, 1, 10, 5, 4, 0])
       expect(RowComposer.retrograde(row).row).to eql [0, 4, 5, 10, 1, 8, 9, 6, 2, 3, 11, 7]
     end
+
+    it 'returns the correct label for the retrograde row' do
+      row = Row.new([11, 3, 10, 9, 1, 0, 6, 2, 5, 8, 4, 7])
+      expect(RowComposer.retrograde(row).label).to eql 'R11'
+    end
   end
 
   describe '#inverse' do
@@ -20,12 +25,22 @@ RSpec.describe RowComposer do
       row = Row.new([1, 0, 5, 7, 9, 8, 4, 11, 2, 10, 6, 3])
       expect(RowComposer.inverse(row).row).to eql [1, 2, 9, 7, 5, 6, 10, 3, 0, 4, 8, 11]
     end
+
+    it 'returns the correct label for the inverse row' do
+      row = Row.new([2, 6, 7, 4, 3, 1, 0, 11, 9, 5, 8, 10])
+      expect(RowComposer.inverse(row).label).to eql 'I2'
+    end
   end
 
   describe '#retrograde_inverse' do
     it 'returns the retrograde inverse of a row' do
       row = Row.new([1, 2, 9, 7, 5, 6, 10, 3, 0, 4, 8, 11])
       expect(RowComposer.retrograde_inverse(row).row).to eql [3, 6, 10, 2, 11, 4, 8, 9, 7, 5, 0, 1]
+    end
+
+    it 'returns the correct label for the retrograde inverse row' do
+      row = Row.new([2, 8, 10, 4, 5, 3, 7, 6, 0, 9, 11, 1])
+      expect(RowComposer.retrograde_inverse(row).label).to eql 'RI2'
     end
   end
 end
