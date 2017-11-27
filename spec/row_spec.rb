1a749ef903fd8ecb933863d475378adb8a26daa9
davidenglishmusic <david@operaonfilm.com>
2017-11-26 20:09:44 -0400

    adds a spec for a row's default label

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -1,8 +1,17 @@
 require 'row'
 
-RSpec.describe Row, '#interval_sequence' do
-  it 'returns the interval sequence' do
-    row = Row.new([7, 4, 2, 5, 8, 0, 9, 3, 11, 6, 10, 1])
-    expect(row.interval_sequence).to eql([-3, -2, 3, 3, -8, 9, -6, 8, -5, 4, -9])
+RSpec.describe Row do
+  describe '#interval_sequence' do
+    it 'returns the interval sequence' do
+      row = Row.new([7, 4, 2, 5, 8, 0, 9, 3, 11, 6, 10, 1])
+      expect(row.interval_sequence).to eql([-3, -2, 3, 3, -8, 9, -6, 8, -5, 4, -9])
+    end
+  end
+
+  describe 'initialization' do
+    it 'creates a default label when none is provided' do
+      row = Row.new([0, 3, 11, 9, 7, 10, 8, 1, 2, 4, 6, 5])
+      expect(row.label).to eql 'P0'
+    end
   end
 end
