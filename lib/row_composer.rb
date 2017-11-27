3474dc9a4e63945d104062c2db5b4c346036b6e1
davidenglishmusic <david@operaonfilm.com>
2017-11-27 19:48:02 -0400

    improves naming within the inverse method

--- lib/row_composer.rb
+++ lib/row_composer.rb
@@ -25,16 +25,15 @@ class RowComposer
   end
 
   def self.inverse(row)
-    inverse = [row.row.first]
-
+    inversed_row = [row.row.first]
     next_note = row.row.first
     row.interval_sequence.each do |interval|
       next_note -= interval
       next_note += 12 if next_note.negative?
       next_note -= 12 if next_note >= 12
-      inverse.push(next_note)
+      inversed_row.push(next_note)
     end
-    Row.new(inverse, :i)
+    Row.new(inversed_row, :i)
   end
 
   def self.retrograde_inverse(row)
